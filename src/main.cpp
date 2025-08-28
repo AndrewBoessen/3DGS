12987ea8f526e333ea6ab7030500316e532dc2f2
Andrew Boessen <boessena@bc.edu>
2025-08-27 21:57:20 -0400

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "dataloader/colmap.hpp"
 #include "gsplat/utils.hpp"
 #include <iostream>
+#include <stdexcept>
 #include <string>
 
 int main(int argc, char *argv[]) {
-  // 1. Check for the correct number of command-line arguments.
+  // Check for the correct number of command-line arguments.
   if (argc != 5) {
     std::cerr << "Usage: " << argv[0]
               << "<path_to_config_file.yaml> <path_to_cameras.bin> <path_to_images.bin> <path_to_points3D.bin>"
@@ -12,14 +13,21 @@ int main(int argc, char *argv[]) {
     return 1; // Return an error code
   }
 
-  // 2. Get file paths from the arguments.
+  // Get file paths from the arguments.
   const std::string config_path = argv[1];
   const std::string cameras_path = argv[2];
   const std::string images_path = argv[3];
   const std::string points3D_path = argv[4];
 
   // Read config file and load parameters
-  ConfigParameters config = parseConfig(config_path);
+  std::cout << "Attempting to read " << config_path << std::endl;
+  try {
+    ConfigParameters config = parseConfig(config_path);
+  } catch (const std::runtime_error &e) {
+    std::cout << "Failed to load config file: " << e.what() << std::endl;
+    return 1; // Return on error
+  }
+  std::cout << "Successfully loaded config file" << std::endl;
 
   std::cout << "Attempting to read COLMAP binary files..." << std::endl;
   std::cout << "Cameras file: " << cameras_path << std::endl;
@@ -27,7 +35,7 @@ int main(int argc, char *argv[]) {
   std::cout << "Points3D file: " << points3D_path << std::endl;
   std::cout << std::endl;
 
-  // 3. Read the cameras.bin file.
+  // Read the cameras.bin file.
   const auto cameras_optional = ReadCamerasBinary(cameras_path);
   if (cameras_optional) {
     const auto &cameras = cameras_optional.value();
@@ -37,7 +45,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // 4. Read the images.bin file.
+  // Read the images.bin file.
   const auto images_optional = ReadImagesBinary(images_path);
   if (images_optional) {
     const auto &images = images_optional.value();
@@ -47,7 +55,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // 5. Read the points3D.bin file.
+  // Read the points3D.bin file.
   const auto points_optional = ReadPoints3DBinary(points3D_path);
   if (points_optional) {
     const auto &points = points_optional.value();
