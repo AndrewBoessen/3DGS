0fe4cb410c28584b345f0fb8d4ed8c51a6e7fa83
Andrew Boessen <boessena@bc.edu>
2025-09-08 11:51:49 -0400

    raster header

--- include/gsplat/raster.hpp
+++ include/gsplat/raster.hpp
@@ -1,3 +1,8 @@
 // raster.cpp
 
 #pragma once
+
+#include "dataloader/colmap.hpp"
+#include "gsplat/gaussian.hpp"
+
+void rasterize_image(Gaussians gaussians, Image image, Camera camera);
---
+++ src/raster.cpp
@@ -0,0 +1,4 @@
+#include "gsplat/raster.hpp"
+#include "gsplat/cuda_forward.hpp"
+
+void rasterize_image(Gaussians gaussians, Image image, Camera camera) {}
